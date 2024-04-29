15b476282916d8746de3f508d207e739127fa483
AriefDaffa <ariefkeren76@yahoo.com>
2024-04-29 16:08:47 +0700

    style: update homepages

--- src/pages/Home/Section/HeroSection.tsx
+++ src/pages/Home/Section/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { SparklesCore } from '@/components/ui/sparkles';
 import { motion } from 'framer-motion';
 import type { FC } from 'react';
 
@@ -10,9 +11,9 @@ const HeroSection: FC<HeroSectionProps> = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="text-center h-[40vh] flex justify-center items-center border my-5 rounded-lg mx-2"
+      className="text-center h-[40vh] flex justify-center items-center my-5 mx-2 relative"
     >
-      <div>
+      <div className="absolute  ">
         <h1 className="scroll-m-20 text-4xl font-medium tracking-tight lg:text-5xl">
           Watching anime <br /> never been this{' '}
           <span className="text-bluePrimary">easy</span>
@@ -20,17 +21,16 @@ const HeroSection: FC<HeroSectionProps> = () => {
         <p className="leading-7 text-muted-foreground [&:not(:first-child)]:mt-2">
           find your favorite anime, or browse trending and latest anime below.
         </p>
-        {/* <div className="mt-4 flex justify-center">
-          <Button
-            variant="outline"
-            className="flex gap-3"
-            onClick={() => navigate('/search')}
-          >
-            <IoSearch />
-            Click here to search
-          </Button>
-        </div> */}
       </div>
+      <SparklesCore
+        id="tsparticlesfullpage"
+        background="transparent"
+        minSize={0.6}
+        maxSize={1.4}
+        particleDensity={100}
+        className="w-full h-full"
+        particleColor="#FFFFFF"
+      />
     </motion.div>
   );
 };
