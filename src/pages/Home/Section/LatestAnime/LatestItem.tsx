7730ddaa5f957d19d61a13ee49332142a9187a04
AriefDaffa <ariefkeren76@yahoo.com>
2024-07-25 11:01:46 +0700

    feat: update home UI

--- src/pages/Home/Section/LatestAnime/LatestItem.tsx
+++ src/pages/Home/Section/LatestAnime/LatestItem.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { FaPlay } from 'react-icons/fa';
+import { useMemo, useState } from 'react';
 import type { FC } from 'react';
 
-import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
 import type { AnimeRecentResults } from '@/services/anime/getRecentAnime/types';
 
 interface LatestItemProps extends AnimeRecentResults {}
@@ -27,22 +27,59 @@ const LatestItem: FC<LatestItemProps> = ({
     setOnHover(false);
   };
 
+  const getMinusOneID = useMemo(() => {
+    const tempEpsIDArr = episodeId.split('-');
+    tempEpsIDArr.pop();
+    tempEpsIDArr.push(String(episodeNumber - 1));
+
+    return tempEpsIDArr.join('-');
+  }, [episodeId, episodeNumber]);
+
   return (
     <div
-      className="flex items-end gap-4 cursor-pointer pt-2 pb-6 border-b-[1px] hover:brightness-75"
-      onClick={() => navigate(`/watch/${id}/${episodeId}`)}
+      className="flex items-end gap-4 pt-2 pb-6 border-b-[1px]"
       onMouseEnter={handleOnMouseHover}
       onMouseLeave={handleOnMouseLeave}
     >
-      <img src={image} alt="" className="w-28 h-40 object-cover" />
-      <div className="w-full flex flex-1">
-        <div className="space-y-2 w-full">
-          <div className="text-lg pr-2 line-clamp-3">{title}</div>
-          <div className="text-xs">Episode {episodeNumber}</div>
-          <Button className="flex gap-4 items-center float-right bg-red-700">
+      <div className="overflow-hidden">
+        <img
+          src={image}
+          alt=""
+          className="w-28 h-40 object-cover cursor-pointer hover:scale-110 hover:brightness-50"
+          onClick={() => navigate(`/detail/${id}`)}
+        />
+      </div>
+      <div className="size-full flex flex-1 ">
+        <div className="space-y-2 size-full">
+          <div className="text-lg pr-2 h-1/2 flex items-end">
+            <div
+              className="line-clamp-3 cursor-pointer hover:brightness-50"
+              onClick={() => navigate(`/detail/${id}`)}
+            >
+              {title}
+            </div>
+          </div>
+          <Separator className="w-1/6" />
+          <div className="text-xs h-1/2 pt-1 space-y-2">
+            <div
+              className="flex justify-between cursor-pointer hover:brightness-50"
+              onClick={() => navigate(`/watch/${id}/${episodeId}`)}
+            >
+              Episode {episodeNumber} <Badge className="bg-red-600">New</Badge>
+            </div>
+            {episodeNumber !== 1 && (
+              <div
+                className="cursor-pointer hover:brightness-50"
+                onClick={() => navigate(`/watch/${id}/${getMinusOneID}`)}
+              >
+                Episode {episodeNumber - 1}
+              </div>
+            )}
+          </div>
+          {/* <Button className="flex gap-4 items-center float-right bg-red-700">
             Play
             <FaPlay />
-          </Button>
+          </Button> */}
         </div>
       </div>
     </div>
--- src/pages/Home/Section/LatestAnime/LatestSkeleton.tsx
+++ src/pages/Home/Section/LatestAnime/LatestSkeleton.tsx
@@ -5,12 +5,12 @@ interface LatestSkeletonProps {}
 const LatestSkeleton: FC<LatestSkeletonProps> = () => {
   return (
     <>
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
-      <div className="h-[65px] bg-primary pulse m-2 rounded-md sm:w-1/2" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
+      <div className="h-[65px] bg-primary pulse m-2 rounded-md" />
     </>
   );
 };
